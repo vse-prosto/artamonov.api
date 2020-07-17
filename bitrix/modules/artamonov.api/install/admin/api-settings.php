2a4f09691c3863aa17758cd20b335693f96d3e24
vse-prosto <info@all-simple.ru>
2020-07-17 13:42:38 +0300

    Поправлены ссылки на страницы админки

--- bitrix/modules/artamonov.api/install/admin/api-settings.php
+++ bitrix/modules/artamonov.api/install/admin/api-settings.php
@@ -1,3 +1,10 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/artamonov.api/admin/pages/api-settings.php';
+$local = $_SERVER['DOCUMENT_ROOT'] . '/local/modules/artamonov.api/admin/pages/api-settings.php';
+$bitrix = $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/artamonov.api/admin/pages/api-settings.php';
+
+if (file_exists($local)) {
+    require_once $local;
+}elseif (file_exists($bitrix)){
+    require_once $bitrix;
+}
--- bitrix/modules/artamonov.api/install/admin/api-support.php
+++ bitrix/modules/artamonov.api/install/admin/api-support.php
@@ -1,3 +1,10 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/artamonov.api/admin/pages/api-support.php';
+$local = $_SERVER['DOCUMENT_ROOT'] . '/local/modules/artamonov.api/admin/pages/api-support.php';
+$bitrix = $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/artamonov.api/admin/pages/api-support.php';
+
+if (file_exists($local)) {
+    require_once $local;
+}elseif (file_exists($bitrix)){
+    require_once $bitrix;
+}
